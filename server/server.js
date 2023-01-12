43cf6fd390f6682533a231423e73cc0186cb7452
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-01-12 15:26:30 -0600

    final updated on get route

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const itemRouter = require(`./routes/item.router`)
+
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-// Create your API routes in a separate file
-// and plug them in here with `app.use()`
+app.use(`items`, itemRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
