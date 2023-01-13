0a6d53dd6240ed4e03936464543260b8dde1ed70
Adam Bergstrom <abx1138@gmail.com>
2023-01-13 09:36:18 -0600

    trying some pull stuff

--- server/routes/item.router.js
+++ server/routes/item.router.js
@@ -16,7 +16,25 @@ router.get('/', (req,res) => {
         })
 });
 
+// router.put('/items/keyid?', (req, res) => {
+//     const idToUpdate = req.params.id;  //figure out keys
+//     const newRender = req.body.render;  //figure out react put
+    
+//     let sqlQuery = `
+//         UPDATE "shopping_list"
+//         SET "is_purchased"=$1
+//         WHERE "id"=$2
+//     `
+//     let sqlValues = [purchaseChange, idToUpdate];
 
+//     pool.query(sqlQuery, sqlValues)
+//         .then((dbRes) => {
+//             res.sendStatus(200);
+//         })
+//         .catch((dbErr) => {
+//             console.log('Error in render PUT', dbErr);
+//         });
+// });
 
 
 module.exports = router; 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import ItemList from '../ItemList/ItemList.jsx'
 import './App.css';
