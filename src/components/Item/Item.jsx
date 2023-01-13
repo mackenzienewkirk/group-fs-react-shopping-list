77becf126b268f312970dbc414f8b84d986c4095
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-01-13 11:27:59 -0600

    decided against singular item folder

--- src/components/Item/Item.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ItemList from '../ItemList/ItemList';
-
-{itemList.map(item => (
-    <Item key={item.id} item={item}/>
-))}
-
-export default Item;
--- src/components/ItemPurchase/ItemPurchase.jsx
+++ src/components/ItemPurchase/ItemPurchase.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+
 function ItemPurchase(getItems) {
     const idToUpdate = $(this).closest('tr').data().id;
     axios.put('/items/:id',{ //key?
